ffb1a691b7a53279bdc7e0532b5ceb57b7caf028
DarshPareek <pareekdarsh0162@gmail.com>
2024-08-17 19:23:34 +0530

    some changes

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,9 @@
   <div class="default-header">
     <div class="container">
       <div class="row">
+        <div class="col-sm-3 col-md-2">
+          <div class="logo"> <a href="index.php"><img src="assets/images/logo.png" alt="image"/></a> </div>
+        </div>
         <div class="col-sm-9 col-md-10">
           <div class="header_info">
          <?php
@@ -14,30 +17,61 @@ $email=$result->EmailId;
 $contactno=$result->ContactNo;
 }
 ?>   
-</div>
-   <?php   if(strlen($_SESSION['login'])==0)
-	{	
-?>
- <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
-<?php }
-else{ 
-// echo "Welcome To Car rental portal";
- } ?>
-          </div>
+
+            <!-- <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">For Support Mail us : </p>
+              <a href="mailto:<?php echo htmlentities($email);?>"><?php echo htmlentities($email);?></a> </div>
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">Service Helpline Call Us: </p>
+              <a href="tel:<?php echo htmlentities($contactno);?>"><?php echo htmlentities($contactno);?></a> </div>
+            <div class="social-follow"> -->
+            
+            </div>
+  
         </div>
       </div>
     </div>
   </div>
+  
+  <!-- Navigation -->
   <nav id="navigation_bar" class="navbar navbar-default">
     <div class="container">
       <div class="navbar-header">
         <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
       </div>
       <div class="header_wrap">
-        <div class="user_login">
+        
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a>    </li>
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+          <li><a href="car-listing.php">Car Listing</a></li>
+          <li><div class="header_search">
+          <div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
+          <form action="search.php" method="post" id="header-search-form">
+            <input type="text" placeholder="Search..." name="searchdata" class="form-control" required="true">
+            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+          </form>
+        </div></li>
+          <li><a href="page.php?type=faqs">FAQs</a></li>
+          <li><a href="contact-us.php">Contact Us</a></li>
+          <li><div class="user_login">
           <ul>
             <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> 
-<?php 
+            <?php   if(strlen($_SESSION['login'])==0)
+	{	
+?>
+ <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+<?php }
+else{ 
+
+// echo "Welcome To Car rental portal";
+ } ?>
+          </div>
+          <?php 
 $email=$_SESSION['login'];
 $sql ="SELECT FullName FROM tblusers WHERE EmailId=:email ";
 $query= $dbh -> prepare($sql);
@@ -64,23 +98,14 @@ foreach($results as $result)
             </li>
           </ul>
         </div>
-        <div class="header_search">
-          <div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
-          <form action="search.php" method="post" id="header-search-form">
-            <input type="text" placeholder="Search..." name="searchdata" class="form-control" required="true">
-            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-          </form>
-        </div>
-      </div>
-      <div class="collapse navbar-collapse" id="navigation">
-        <ul class="nav navbar-nav">
-          <li><a class="main-logo" href="index.php"><img  src="assets/images/logo.png" alt="image"/></a></li>
-          <li><a href="car-listing.php">Car Listing</a>
-          <li><a href="page.php?type=faqs">FAQs</a></li>
-          <li><a href="contact-us.php">Contact Us</a></li>
+        </li>
+          
+
         </ul>
       </div>
+      
     </div>
   </nav>
   <!-- Navigation end --> 
+  
 </header>
